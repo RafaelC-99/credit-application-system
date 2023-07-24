4c61ac0e25e967a0e9bdc22faeb2355dc6f7a455
Rafael A <rafaelcardosocatuti@gmail.com>
2023-07-24 12:21:53 -0300

    repository implemented

--- src/main/kotlin/me/dio/creditapplicationsystem/entity/Customer.kt
+++ src/main/kotlin/me/dio/creditapplicationsystem/entity/Customer.kt
@@ -1,12 +1,25 @@
 package me.dio.creditapplicationsystem.entity
 
+import jakarta.annotation.Generated
+import jakarta.persistence.*
+
+@Entity
 data class Customer(
-    var firstName: String = "",
-    var lastName: String = "",
-    var cpf: String,
-    var email: String = "",
-    var password: String = "",
-    var address: Address = Address(),
+    @Column(nullable = false) var firstName: String = "",
+    @Column(nullable = false) var lastName: String = "",
+    @Column(nullable = false, unique = true) var cpf: String,
+    @Column(nullable = false, unique = true) var email: String = "",
+    @Column(nullable = false) var password: String = "",
+    @Column(nullable = false) @Embedded var address: Address = Address(),
+
+    @Column(nullable = false)
+    @OneToMany(
+        fetch = FetchType.LAZY,
+        cascade = [CascadeType.REMOVE, CascadeType.PERSIST],
+        mappedBy = "customer")
     var credits: List<Credit> = mutableListOf(),
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null
 )
